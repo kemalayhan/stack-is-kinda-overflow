ab988bcabfa5dd43dbfcf8465dee7c625c77e499
kemal ayhan <kemalayhan013@gmail.com>
2020-09-26 14:03:52 +0300

    fixed static

--- base/settings.py
+++ base/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        "static",
+]
 
 AUTH_USER_MODEL='users.User'
 
